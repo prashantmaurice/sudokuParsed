c18925a6971bb4513e7708e390161ad0e67360f0
Prashant Maurice <sabertoothmaurice@gmail.com>
2015-08-16 17:55:15 +0530

    added Basic android project with JNI library and openCV library included

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # sudokuParsed
+
+To use this project just follow the below instructions :
+
+
+# Contributors
+
+Prashant Maurice
+Umang Ganvir
+Krishna Chanitanya
---
+++ app/src/main/java/com/maurice/app/sudokuapp/ImageParser/ImageParser.java
@@ -0,0 +1,12 @@
+package com.maurice.app.sudokuapp.ImageParser;
+
+/**
+ * Created by maurice on 16/08/15.
+ */
+public class ImageParser {
+    public ImageParser(){
+
+    }
+
+
+}
